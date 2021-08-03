46dd1bca4ef93f8ead54c6f769f28efcc0e3e4f9
Cristian Martinez <dbautista2681@gmail.com>
2021-08-03 12:58:28 -0500

    States from public to private.

--- ActualPokemon.cs
+++ ActualPokemon.cs
@@ -1,5 +1,5 @@
 public class ActualPokemon: PokedexItem { 
-    public string ability;
+    private string ability;
     public string Ability {get {return ability;} set {ability = value;} }
 
     public ActualPokemon(string ability, string color): base(color) {
--- PokedexItem.cs
+++ PokedexItem.cs
@@ -1,6 +1,6 @@
 public class PokedexItem {
 //states
-    string color;
+    private string color;
     public string Color { get => color; set => color = value; }
 
     public PokedexItem(string color)
